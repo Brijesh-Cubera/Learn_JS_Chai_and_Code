cee368d6811002c469d042472fde8300bd7bbf63
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2023-12-29 10:49:30 +0000

    updaTE

--- 01_basic/02_dataType.js
+++ 01_basic/02_dataType.js
@@ -2,4 +2,30 @@
 
 // alert(3+3)   //this is working in brouser but her we use nodeJs
 
-console.log(3+3);
+console.log(3+3); 
+
+let name = "Brijesh";
+let age = 27;
+let isLoggedIn = true;
+
+/* Type of Data Type:-
+
+1) Number => any integer upto range 2^53.
+2) bigint => when number is to big that time we use bigint normally it is not use
+3) string => ""
+4) boolean => true/false
+5) null => standalone value
+6) undefine => value not define
+7) symbol => to find uniqueness
+8) objects =>
+
+*/
+
+// when we find typeof undefined it give undefined
+// But when we find typeof null it give object
+
+console.log(typeof"brijesh"); // string
+console.log(typeof age); // number
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof undefined); // undefine
+console.log(typeof null); // object
