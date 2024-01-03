f88f346d32d6658e45b713ad25c2228dc2da7d43
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-03 12:34:57 +0000

    update

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -24,3 +24,43 @@ console.log(newGame.length); // 13
 console.log(newGame.toUpperCase()); // BRIJESH PATEL
 console.log(newGame.charAt(3)); // j  at place 2 j is available
 console.log(newGame.indexOf("j")); // j is available at 3nd position 
+
+
+const gamerName = new String("callONDutty");
+
+const stringName = gamerName.substring(0, 4); // call the alphabet comes under 0 to 3 
+
+console.log(stringName) // call it start from 0 to 3
+
+
+const anotherString = gamerName.slice(-8, 4); // call the alphabet from -8 to 4 in slice method we call negative character
+
+console.log(anotherString);
+
+
+const nextString = "   Rohan  "  
+console.log(nextString) //    Rohan   it give blank space from start and end
+console.log(nextString.trim()) // it does not five any space it give only character 
+
+const newName1 = "Munna_Chor_Hai"
+
+console.log(newName1.replace("Chor", "Gandu")); // Munna_Gandu_Hai
+
+console.log(newName1.includes("Chor")); // true
+console.log(newName1.includes("MC")); // false
+
+// convert string to array
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[3]); // Expected output: "fox"
+
+const chars = str.split('');
+console.log(chars[8]); // Expected output: "k"
+
+const strCopy = str.split();
+console.log(strCopy); // Array ["The quick brown fox jumps over the lazy dog."]
+
+const strCopy2 = str.split('');
+console.log(strCopy); // Array ["The quick brown fox jumps over the lazy dog."]
