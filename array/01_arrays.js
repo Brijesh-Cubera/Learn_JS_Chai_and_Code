7da2eeff189b7f0c83f63d7273c2d8818cd0ae32
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-04 17:57:44 +0000

    update

--- array/01_arrays.js
+++ array/01_arrays.js
@@ -2,5 +2,79 @@
 // arrays are not associative arrays and so, array elements cannot be accessed using 
 // arbitrary strings as indexes, but must be accessed using nonnegative integers as indexes.
 
-const myArray
- = [0,1,2,3,4,5,6,]
+const myArray = [0,1,2,3,4,5,6,]
+const myHeros = ["Shaktiman", "Aryaman", "Doga", "Nagraj", "Bakelal", "Dhruv"]
+
+
+console.log(myArray[3]); // 3
+console.log(myHeros[4]); // Bakelal
+const myArray2 = new Array(1,2,3,4,5) //its automatically transfer into array
+console.log(myArray2); // [ 1, 2, 3, 4, 5 ] 
+
+
+// const numbers = [1,2,3,4,5] // when we inspect in web and write this and ifprint then below all things appear 
+// console.log(numbers) // below all method perform in Array
+// (5) [1, 2, 3, 4, 5]
+//     0: 1
+//     1: 2
+//     2: 3
+//     3: 4
+//     4: 5
+// length: 5
+// [[Prototype]]: Array(0)
+        // at: ƒ at()
+        // concat: ƒ concat()
+        // constructor: ƒ Array()
+        // copyWithin: ƒ copyWithin()
+        // entries: ƒ entries()
+        // every: ƒ every()
+        // fill: ƒ fill()
+        // filter: ƒ filter()
+        // find: ƒ find()findIndex: ƒ findIndex()
+        // findLast: ƒ findLast()
+        // findLastIndex: ƒ findLastIndex()
+        // flat: ƒ flat()
+        // flatMap: ƒ flatMap()
+        // forEach: ƒ forEach()
+        // includes: ƒ includes()
+        // indexOf: ƒ indexOf()
+        // join: ƒ join()
+        // keys: ƒ keys()
+        // lastIndexOf: ƒ lastIndexOf()
+        // length: 0map: ƒ map()
+        // pop: ƒ pop()
+        // push: ƒ push()
+        // reduce: ƒ reduce()
+        // reduceRight: ƒ reduceRight()
+        // reverse: ƒ reverse()
+        // shift: ƒ shift()
+        // slice: ƒ slice()
+        // some: ƒ some()
+        // sort: ƒ sort()
+        // splice: ƒ splice()
+        // toLocaleString: ƒ toLocaleString()
+        // toReversed: ƒ toReversed()
+        // toSorted: ƒ toSorted()
+        // toSpliced: ƒ toSpliced()
+        // toString: ƒ toString()
+        // unshift: ƒ unshift()
+        // values: ƒ values()
+        // with: ƒ with()
+        // Symbol(Symbol.iterator): ƒ values()
+        // Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Object
+            // [[Prototype]]:Object
+                // constructor: ƒ Object()
+                // hasOwnProperty: ƒ hasOwnProperty()
+                // isPrototypeOf: ƒ isPrototypeOf()
+                // propertyIsEnumerable: ƒ propertyIsEnumerable()
+                // toLocaleString: ƒ toLocaleString()
+                // toString: ƒ toString()
+                // valueOf: ƒ valueOf()
+                // __defineGetter__: ƒ __defineGetter__()
+                // __defineSetter__: ƒ __defineSetter__()
+                // __lookupGetter__: ƒ __lookupGetter__()
+                // __lookupSetter__: ƒ __lookupSetter__()
+                // __proto__: (...)
+                // get __proto__: ƒ __proto__()
+                // set __proto__: ƒ __proto__()
+// Above all methode can be perform in Array
