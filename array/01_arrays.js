c178634977adca48496ea72afe4b1ab8782c1021
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-04 16:05:21 +0000

    update

---
+++ array/01_arrays.js
@@ -0,0 +1,6 @@
+// Array :- JavaScript arrays are resizable and can contain a mix of different data types.
+// arrays are not associative arrays and so, array elements cannot be accessed using 
+// arbitrary strings as indexes, but must be accessed using nonnegative integers as indexes.
+
+const myArray
+ = [0,1,2,3,4,5,6,]
