4d4bcc14b49cefee3273e7220b16976ebf26ea27
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-04 11:50:05 +0000

    update

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+/*
+const score = 400 // simple way to define a number 
+console.log(score);
+
+// exclusive define a number means it should be only a no
+
+const balance = new Number(100);
+console.log(balance); //[Number: 100]
+
+console.log(balance.toString()); // 100 //number to string and when we change into number to string then we also access all string data like length of balance
+
+console.log(balance.toString().length); // 3
+console.log(balance.toFixed(2)); // 100.00 after decimal how many no you want
+
+const otherNumber = 23.6789
+console.log(otherNumber.toPrecision(3)); //23.7//it give precise value in 3 numbers counting start from start we have only 2 digit before decimal so it take 1 decimal value in doundoff form
+
+const otherNumber1 = 123.6789
+console.log(otherNumber1.toPrecision(3)); //124 // it give precise value in 3 numbers counting start from starting we have alreasdy 3 digit before decimal so its roundoff in 3 digit
+
+const hundreds = 100000
+
+console.log(hundreds.toLocaleString()); //100,000 //by US standard
+console.log(hundreds.toLocaleString('en-IN')); // 1,00,000 //by Indian standard
+*/
+
+//++++++++++++++++++++ Maths +++++++++++++++++++++++
