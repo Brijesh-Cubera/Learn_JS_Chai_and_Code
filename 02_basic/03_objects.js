e65631ca8b1d135f58d9997bf97ed18b251a8eed
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-05 10:16:11 +0000

    update

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,48 @@
+// singleton:- when we create through constructor then its create a singleton object
+
+// when we declair through literal then singleton not created
+
+// object literals
+
+const jsUser1 = {} // empty object
+
+Object.create    // we also create object through and this type of object is called object constructor method and due to this singleton created
+
+const jsUser2 = {
+    name: "Brijesh"
+}
+
+myArray = ["h", "i"]
+myArray[0] // we have only option to access the value of myArray 
+
+// so we use object wher we have to declair key as well as value
+
+const mysym = Symbol("key1") // for Symbol 
+
+const jsUser = {
+    name: "Brijesh",
+    "full name": "Brijesh Patel",
+    age: 27,
+    mysym1: "mykey1", // it does not provide the symbol so we have to rite mySym inside []
+    [mysym]: "mykey2",
+    location: "Bangalore",
+    email: "ibrijeshpatel@outlook.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "saturday"]
+
+}
+
+console.log(jsUser.name); // Brijesh // every one chose this to access the value but is wan not a correct way to find the value
+// console.log(jsUser.full name); // we never get through this so we use square bracket to get the result
+console.log(jsUser["name"]); // Brijesh // every one chose this to access the value but is wan not a correct way to find the value
+console.log(jsUser["full name"]); // Brijesh Patel // every one chose this to access the value but is wan not a correct way to find the value
+
+console.log(typeof jsUser["mysym1"]); // string
+console.log(jsUser[mysym]); // mykey2
+
+jsUser.email = "brijeshnitap@gmail.com"
+console.log(jsUser["email"]); // Brijesh Patel // every one chose this to access the value but is wan not a correct way to find the value
+
+
+
+ 
